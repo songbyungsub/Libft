e0b9276f4f86d8174c78809f36bb5245238e4fe7
byung-sub <98787306+songbyungsub@users.noreply.github.com>
2022-08-25 10:47:28 +0900

    Add files via upload

---
+++ ft_memset.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: byusong <byusong@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/08 10:26:41 by byusong           #+#    #+#             */
+/*   Updated: 2022/08/13 14:29:03 by byusong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*temp;
+
+	temp = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+		temp[i++] = (unsigned char)c;
+	return ((void *)temp);
+}	
