991ded6b95f8d30e9d4cbb1b2ae28c282a394c9a
byung-sub <98787306+songbyungsub@users.noreply.github.com>
2022-09-06 02:46:07 +0900

    Add files via upload

---
+++ ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: byusong <byusong@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/11 12:39:49 by byusong           #+#    #+#             */
+/*   Updated: 2022/08/13 14:28:43 by byusong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*ret;
+
+	ret = (t_list *)malloc(sizeof(t_list));
+	if (ret == NULL)
+		return (NULL);
+	ret->content = content;
+	ret->next = NULL;
+	return (ret);
+}
